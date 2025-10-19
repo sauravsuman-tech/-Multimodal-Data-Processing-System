2a8496994460c0b0c577c180c857060844f99f65
Saurav Kumar Suman <sauravsuman0299@gmail.com>
2025-10-19 17:51:16 +0530

    Delete ocr.py

--- ocr.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-import pytesseract
-from pathlib import Path
-
-def ocr_image_file(path: Path) -> str:
-    img = Image.open(str(path))
-    text = pytesseract.image_to_string(img)
-    return text
